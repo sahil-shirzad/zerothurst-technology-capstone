6d1d382f7c95967abd204d1c8f03d73d96649f3c
sahil-shirzad <273885458+sahil-shirzad@users.noreply.github.com>
2026-06-06 19:02:14 -0700

    Update main.tf

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,1 +1,33 @@
-# Infrastructure Terraform Configuration
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# VPC
+
+resource "google_compute_network" "securetrack_vpc" {
+  name                    = "securetrack-vpc"
+  auto_create_subnetworks = true
+}
+
+# Cloud SQL
+
+resource "google_sql_database_instance" "postgres" {
+  name             = "securetrack-db"
+  region           = var.region
+  database_version = "POSTGRES_15"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+# Secret Manager
+
+resource "google_secret_manager_secret" "db_password" {
+  secret_id = "db-password"
+
+  replication {
+    auto {}
+  }
+}
